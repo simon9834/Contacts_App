c5ca4f5983b7ef3eb3687e210301f4c945136eb0
Šimon Juda Hloska <hloska@spsejecna.cz>
2025-03-19 10:38:27 +0100

    Add project files.

---
+++ Cviceni-19-03-2025/Contact.cs
@@ -0,0 +1,14 @@
+﻿
+public class Contact
+{
+    private string name;
+    private int phoneNum;
+    private string email;
+
+    public Contact(string name, int phoneNum, string email)
+    {
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+}
---
+++ Cviceni-19-03-2025/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni_19_03_2025
+{
+    internal class MenuItem
+    {
+    }
+}
